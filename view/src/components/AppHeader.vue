2fbd5a1da06e2f21816e0441952ea4f4a726d677
Raitraut <fixednoise113@gmail.com>
2020-06-12 16:11:20 +0300

    create project

---
+++ view/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="header">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppHeader"
+    }
+</script>
+
+<style scoped>
+    @import "src/assets/css/header.css";
+</style>
